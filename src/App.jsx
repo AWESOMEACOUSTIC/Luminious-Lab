a1b37fea2c0226d5cb13de4ce8e31808cdbbd57b
Aditya <adityapanigrahy98@gmail.com>
2025-06-26 12:12:26 +0530

    Footer is added

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import CardsGrid from './components/CardContent'
 import Banner1 from './components/Banner1'
 import Benefits from './components/Benefits'
 import Data from './components/Data'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className='w-full min-h-screen py-[0.4vw] px-[1vw] bg-[#fcf8f1]'>
+    <div className='w-full min-h-screen py-[0.4vw] px-[1.4vw] bg-[#fcf8f1]'>
       <Hero/>
       <Content />
       <CardsGrid />
       <Banner1/>
       {/* <Benefits /> */}
       <Data />
+      <Footer/>
     </div>
   )
 }
